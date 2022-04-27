3c219cabcf1c984c9c32e160a952536c74a6f868
Nindaime <41658304+Nindaime@users.noreply.github.com>
2022-04-27 14:47:15 +0100

    Update Gradle

--- openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary343/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Admin\\Desktop\\AutoSelfie\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Dell\\Documents\\GitHub\\AutoSelfie\\openCVLibrary343\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
